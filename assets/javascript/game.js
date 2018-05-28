51fd091c3038052bfa443273385cb03b5fb6737b
Hooch <nativetongues09@gmail.com>
2018-05-28 18:34:47 -0400

    04-Crystal_Collector

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+1. make array of random numbers (19-120)
+
+2. use math random to pick random number from array 
+
+3. each four buttons html will link to javascript onclick events
+
+4. each button is assigned a different value to represent a the points awarded (var blue = 100pts, var white = 200pts)
+
+5. if player score = target amount 
+	then win counter = +1 
+
+6. if player score > target amount
+	then loss counter = -1
+	then reset() function
