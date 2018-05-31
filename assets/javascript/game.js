f809fc79a3debc45322586a63f1ef40db18868db
Hooch <nativetongues09@gmail.com>
2018-05-30 21:56:54 -0400

    04-Crystal_Collector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,38 @@
-1. make array of random numbers (19-120)
+$(document).ready(function() {
+   
+    var playerScore = playerScoreRandom()
+	var crystalOneValue = crystalRandom()
+	var crystalTwoValue = crystalRandom()
+	var crystalThreeValue = crystalRandom()
+	var crystalFourValue = crystalRandom()
+	var totalCrystalValue = 0
+	
+	
+	//random value 19-120 for target.
+	function playerScoreRandom(min, max) {
+	return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	}
+	
+	//random value 1-12 for crystals.
+	
+	function crystalRandom(min, max) {
+	return Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	}
+		
+	console.log(parseInt(crystalOneValue) + parseInt(crystalTwoValue))
+	
+	console.log(crystalOneValue);
+	console.log(crystalTwoValue);
+	console.log(crystalThreeValue);
+	console.log(crystalFourValue);
+	console.log("-------------")
+	console.log(playerScore)
+	
+	
+	
+	}) 
+
+/*1. make array of random numbers (19-120)
 
 2. use math random to pick random number from array 
 
@@ -12,3 +46,4 @@
 6. if player score > target amount
 	then loss counter = -1
 	then reset() function
+
